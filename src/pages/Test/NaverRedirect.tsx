37c37b0e2e89913d2bdeaa845317fe9b6e8b382e
김동훈A <agmon5959@douzone.com>
2022-09-22 23:43:06 +0900

    naver login test setting

---
+++ src/pages/Test/NaverRedirect.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+type AppProps = {
+    name: string
+}
+
+const NaverRedirect: React.FC<AppProps> = (props) => {
+
+    return (
+        <div>
+            NaverRedirect!
+        </div>
+    )
+}
+
+export default NaverRedirect;
