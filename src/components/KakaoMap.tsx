8123f45f3da6e5e98b054390217ea930d08797dc
Kim Jeong In <gsdgf1324@naver.com>
2022-10-02 21:53:01 +0900

    빌드 테스트

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -60,7 +60,7 @@ const KakaoMap = () => {
   return (
     <>
       <KakaoMapSearchWrapper>
-        <KakaoMapSearch placeholder="아자아자 수진님 화이팅" />
+        <KakaoMapSearch placeholder="아자아자 수진님 화이팅aa" />
       </KakaoMapSearchWrapper>
 
       <KakaoMapWrapper id="map" ref={KakaoMapRef} />
