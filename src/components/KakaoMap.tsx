7eb3e10fd26d12e2db628218a26025231a114423
김동훈A <agmon5959@douzone.com>
2022-10-02 22:06:25 +0900

    build test

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -60,7 +60,7 @@ const KakaoMap = () => {
   return (
     <>
       <KakaoMapSearchWrapper>
-        <KakaoMapSearch placeholder="아자아자 수진님 화이팅x2*3*3" />
+        <KakaoMapSearch placeholder="아자아자 수진님 화이팅x2*3*31" />
       </KakaoMapSearchWrapper>
 
       <KakaoMapWrapper id="map" ref={KakaoMapRef} />
