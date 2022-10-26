c6f9a83ea2ea5958e14d6bbcb25caea4a8d2f07f
Kim Jeong In <gsdgf1324@naver.com>
2022-10-26 17:45:47 +0900

    상호 리뷰 등록 추가

---
+++ src/components/Modal/ReviewModal/ReviewModal.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import ReviewModalHeader from './ReviewModalHeader';
+import ReviewContent from './ReviewContent';
+import ReviewWrite from './ReviewWrite';
+
+const ReviewModal = () => {
+  return (
+    <ReviewModalWrapper>
+      <ReviewModalHeader />
+      <ReviewContent />
+      <ReviewWrite />
+    </ReviewModalWrapper>
+  );
+};
+
+export default ReviewModal;
+
+const ReviewModalWrapper = styled.div`
+  width: 610px;
+  height: 800px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 10px 30px 10px 30px;
+`;
---
+++ src/components/Modal/ReviewModal/index.ts
@@ -0,0 +1,1 @@
+import { default as ReviewModal } from './ReviewModal';
