d9c67af2484fb94c0326ae77e98fe6fa3447bc73
김동훈A <agmon5959@douzone.com>
2022-11-06 18:55:28 +0900

    환경변수 추가

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-// require("dotenv").config({
-//   path: `./config/.env.development`,
-// });
-
-// module.exports = {
-//   reactStrictMode: false,
-//   swcMinify: true,
-
-//   // CORS 오류 해결 소스로 작성하였으나
-//   // API_KEY 숨기는 용도로 추후에 재사용 예정
-//   // async rewrites() {
-//   //   return [
-//   //     {
-//   //       source: "/:path*",
-//   //       destination: "http://101.101.217.55:8080/:path*",
-//   //     },
-//   //   ]
-//   // },
-//   env: {
-//     MY_VAR: process.env.MY_VAR
-//   },
-  // basePath: process.env.BASE_URL,
-// };
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 
@@ -31,4 +11,4 @@ const nextConfig = {
   env: result.parsed,
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
