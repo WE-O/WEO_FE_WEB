c3625315717a42cf9c0a68c0330961add372c8ee
김동훈A <agmon5959@douzone.com>
2022-11-21 14:42:50 +0900

    ssl 적용

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const { createServer } = require("https");
+const http = require("http");
 const { parse } = require("url");
 const next = require("next");
-const fs = require("fs");
-
-const hostname = "siksikmulmul.shop";
-const port = 3000;
 
 const dev = process.env.NODE_ENV !== "production";
-console.log('dev>',dev);
-const app = next({ dev, hostname, port });
-console.log('1111');
+const app = next({ dev });
 const handle = app.getRequestHandler();
-console.log('2222');
 
-const httpsOptions = {
-    // ca: fs.readFileSync('/etc/letsencrypt/live/siksikmulmul.shop/fullchain.pem'),
-    key: fs.readFileSync("/etc/letsencrypt/live/siksikmulmul.shop/privkey.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/live/siksikmulmul.shop/cert.pem"),
-};
-console.log('33333');
-console.log('httpsOptions>', httpsOptions);
+const PORT = 3000;
 
 app.prepare().then(() => {
-    createServer(httpsOptions, async (req, res) => {
-        try {
+    http
+        .createServer((req, res) => {
+            // Be sure to pass `true` as the second argument to `url.parse`.
+            // This tells it to parse the query portion of the URL.
             const parsedUrl = parse(req.url, true);
-            await handle(req, res, parsedUrl);
-        } catch (err) {
-            console.error("Error occurred handling", req.url, err);
-            res.statusCode = 500;
-            res.end("internal server error");
-        }
-    }).listen(port, (err) => {
-        if (err) throw err;
-        console.log(`> Ready on https://${hostname}:${port}`);
-    });
+            handle(req, res, parsedUrl);
+        })
+        .listen(PORT, (err) => {
+            if (err) throw err;
+            console.log(`> Ready on http://localhost:${PORT}`);
+        });
+
+
+    const https = require("https");
+    const fs = require("fs");
+    const options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/siksikmulmul.shop/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/siksikmulmul.shop/cert.pem')
+    };
+    https
+        .createServer(options, function (req, res) {
+            // Be sure to pass `true` as the second argument to `url.parse`.
+            // This tells it to parse the query portion of the URL.
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl);
+        })
+        .listen(PORT + 1, (err) => {
+            if (err) throw err;
+            console.log(`> Ready on https://localhost:${PORT + 1}`);
+        });
 });
