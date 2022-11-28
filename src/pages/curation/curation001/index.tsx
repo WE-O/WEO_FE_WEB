2e7a42d6afe9ad946de2107c28ca1d48df7b2ed3
김동훈A <agmon5959@douzone.com>
2022-11-28 22:42:29 +0900

    대한민국 화이팅

--- src/pages/curation/curation001/index.tsx
+++ src/pages/curation/curation001/index.tsx
@@ -21,41 +21,43 @@ const Curation001 = () => {
                     <h1>테라리움에 대해 들어보신 적 있나요?</h1>
                     테라리움이란 밀폐된 유리 그릇이나 아가리가 작은 유리병 따위의 안에 작은 식물을 재배하는 방법을 뜻합니다.<br />
                     책상위 작은 정원으로 많이들 알고 계실 거에요!<br />
-
+                    
                     <div>
+                        <br />
                         <Image src={curImg_1.src} width={"450px"} height={"450px"} />
+                        &nbsp;&nbsp;&nbsp;
                         <Image src={curImg_2.src} width={"450px"} height={"450px"} />
                     </div>
-                    
-                    
-
+                    이미지 출처 : https://blog.naver.com/ysqueen1/222890984348
+                    <br /><br />
                     <h1>혹시, 짱구 좋아하시나요?</h1><br />
                     <div>
                         <Image src={curImg_3.src} width={"450px"} height={"450px"} />
+                        &nbsp;&nbsp;&nbsp;
                         <Image src={curImg_4.src} width={"450px"} height={"450px"} />
+                        <br /><br />
                     </div>
+                    {/* <div>
+                        https://blog.naver.com/dayooki/220793529817
+                        https://www.idus.com/w/product/e659adba-acae-4e76-82df-708e9d48527d
+                    </div> */}
                     요즘은 평소 좋아하는 캐릭터의 피규어와 식물을 결합해 아기자기한 나만의 테라리움을 꾸미는 분들이 많이 계시답니다. <br />
-
                     <h1>테라리움 만드는 방법? 어렵지 않아요!</h1>
                     사실 유리 병에 모래를 부어주고, 병 안에서 자라게될 미끼를 올려주면 끝이 난답니다.<br />
                     더 자세하게, 어떻게 테라리움을 만들 수 있는지 영상으로 알아볼까요? 유튜브에서 가장 인기가 많은 영상 2개를 가지고 왔습니다.<br />
-
                     <br />
                     - (초급) 다이소 재료로 테라리움 만들기<br />
                     <a href="https://www.youtube.com/watch?v=qj2g11iZnFw">https://www.youtube.com/watch?v=qj2g11iZnFw</a><br />
-
                     <br />
                     - (중급) 본격적으로 테라리움 만들기<br />
                     <a href="https://www.youtube.com/watch?v=qj2g11iZnFw">https://www.youtube.com/watch?v=qj2g11iZnFw</a><br />
                     요즘은 테라리움 만들기 kit를 구매하여서 바로 만들어 볼 수도 있습니다.
-
-
                     <h1>테라리움, 곰팡이 생길까 걱정되시나요?</h1>
                     걱정 마세요! 딱 3가지만 기억해주시면 오래오래 함께 할 수 있답니다.<br />
                     첫번째, 이끼가 건조해졌을 때 분부기로 물을 뿌려주세요!<br />
                     두번째, 테라리움 안까지 공기가 통하게 해주시면 곰팡이 걱정이 싹 사라진답니다!<br />
                     마지막 세번째 이끼의 특성 상 햇빛이 잘 통하는 창가보다는 내 책상 위와 같은 서늘한 곳에 놓아주시면 더 좋습니다.<br />
-
+                    <br />
                     <h1>테라리움에 대한 궁금증이 해소되셨나요?</h1>
                     <hr/>
                     긴 글 읽어주셔서 감사합니다. 식식물물.<br />
@@ -82,7 +84,7 @@ const CurationInnerBox = styled.div`
         // font-weight: Noto Sans CJK KR-Regular;
         font-family: 'Noto Sans CJK KR';
         font-size: 16px;
-        letter-spacing: 0.8px;
+        letter-spacing: -0.1px;
         line-height: 34px;
     }
 `
